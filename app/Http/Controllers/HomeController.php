0fac878b7cb214067b5548bcd982b753be33172f
Osvaldo Filho <osvaldo_filho@live.com>
2024-12-10 17:27:01 -0300

    corousel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     public function home()
     {     
-        $products = Product::paginate(80);
+        $products = Product::all();
         return view('home', [
             'products' => $products
         ]);
--- resources/views/admin/teste.blade.php
+++ resources/views/admin/teste.blade.php
@@ -6,197 +6,257 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-   
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="app/css/index.css">
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/js/bootstrap.min.js">
-  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css">
-</head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="app/css/index.css">   
 </head>
 
 <body>
-    <div class="block5 col-12">
-        <div class="card">
-          <div class="card-header">
-            Evolução dos Embarques
-          </div>
-          <table class="table" id="mydata">
-            <thead>
-              <tr>
-                <th>Rota</th>
-                <th>Localidade</th>
-                <th>Horário Previsto</th>
-                <th>Horario Real</th>
-                <th>Status</th>
-                <th>Columm</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>120</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>130</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>105</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-              <tr>
-                <td>100</td>
-                <td>Juazeiro</td>
-                <td>18:00</td>
-                <td>18:15</td>
-                <td>Finalizado c/ Atraso</td>
-                <td>Columm</td>
-              </tr>
-            </tbody>
-          </table>
-    
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+              <table class="table" id="mydata">
+                <thead>
+                    <tr>
+                        <th>PRIMEIRO SLIDE</th>
+                        <th>Localidade</th>
+                        <th>Horário Previsto</th>
+                        <th>Horario Real</th>
+                        <th>Status</th>
+                        <th>Columm</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>120</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>130</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>105</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+
+                </tbody>
+            </table>
+            </div>
+            <div class="carousel-item">
+              <table class="table" id="mydata">
+                <thead>
+                    <tr>
+                        <th>SEGUNDO SLIDE</th>
+                        <th>Localidade</th>
+                        <th>Horário Previsto</th>
+                        <th>Horario Real</th>
+                        <th>Status</th>
+                        <th>Columm</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>120</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>130</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>105</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+
+                </tbody>
+            </table>
+            </div>
+            <div class="carousel-item">
+              <table class="table" id="mydata">
+                <thead>
+                    <tr>
+                        <th>TERCEIRO SLIDE</th>
+                        <th>Localidade</th>
+                        <th>Horário Previsto</th>
+                        <th>Horario Real</th>
+                        <th>Status</th>
+                        <th>Columm</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>120</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>130</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>105</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                    <tr>
+                        <td>100</td>
+                        <td>Juazeiro</td>
+                        <td>18:00</td>
+                        <td>18:15</td>
+                        <td>Finalizado c/ Atraso</td>
+                        <td>Columm</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
         </div>
-      </div>
-    
-      <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"
-        crossorigin="anonymous"></script>
-      <script src="app/js/renderer.js"></script>
-      <script src="app/connection.js"></script>
-      <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-      <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"
-        crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
-        crossorigin="anonymous"></script>
-      <script>
-        $('#mydata').dataTable({
-          "searching": false,
-          "bLengthChange": false,
-          "pageLength": 12,
-          "bInfo": false,
-          "ordering": false
-        });
-      </script>
+    </div>
 </body>
-
 </html>
-
