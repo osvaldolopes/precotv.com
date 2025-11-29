2777175c37586c1e6a7e95ca8599c588111c4710
Osvaldo Filho <osvaldo_filho@live.com>
2025-11-29 10:38:24 -0300

    model view

--- app/Models/Mercador_view.php
+++ app/Models/Mercador_view.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Mercador_view extends Model
 {
     use HasFactory;
+
+
+    //protected $connection = 'db_precotv';
+    protected $table = 'mercador_precos_view';
+
+    protected $fillable = [
+        'prod_cod',
+        'prod_description',
+        'valor',
+        'PrecoOferta',
+        'departament_id',
+        'prod_checked',
+    ];
 }
